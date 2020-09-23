e504b34fadd3be69b617d4ab2f5cfe0c780fe37f
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-09-23 16:49:44 -0300

    Questão URI 1042

---
+++ uri/uri_1042.py
@@ -0,0 +1,11 @@
+# Captando input
+nums = [int(x) for x in input().split()]
+
+# Imprime em ordem crescente
+for num in sorted(nums):
+    print(num)
+print("")
+
+# Imprime na ordem de leitura
+for num in nums:
+    print(num)
