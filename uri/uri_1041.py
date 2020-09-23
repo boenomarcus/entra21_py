f5dee52f10de0eaf2463381ad319e3944857cbd0
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-09-23 11:53:24 -0300

    Questão URI 1041

---
+++ uri/uri_1041.py
@@ -0,0 +1,18 @@
+# Captando par de coordenadas x-y
+x, y = map(float, input().split())
+
+# Detectando posicao no plano cartesiano (quadrante)
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+elif x > 0 and y > 0:
+    print("Q1")
+elif x < 0 and y > 0:
+    print("Q2")
+elif x < 0 and y < 0:
+    print("Q3")
+else:
+    print("Q4")
