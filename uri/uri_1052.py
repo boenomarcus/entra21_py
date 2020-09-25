8a49768af4880da16e4ab553a1e4259cfd61fd5a
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-09-24 22:05:14 -0300

    Questão URI 1052

---
+++ uri/uri_1052.py
@@ -0,0 +1,21 @@
+# Capta mes no formato numerico
+mes = int(input())-1
+
+# Declara lista com os meses
+meses = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+# Apresenta resultado
+print(meses[mes])
