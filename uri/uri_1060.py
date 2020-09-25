e51d2f606bb86676f0b5966cbfeaf4cf2bd849d4
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-09-24 22:16:59 -0300

    Questão URI 1060

---
+++ uri/uri_1060.py
@@ -0,0 +1,12 @@
+# Captando valores nao-nulos
+lst = []
+n = 6
+while n > 0:
+    num = float(input())
+    if num != 0:
+        if num > 0:
+            lst.append(num)
+        n -= 1
+
+# Apresentado numero de valores positivos
+print(f"{len(lst)} valores positivos")
