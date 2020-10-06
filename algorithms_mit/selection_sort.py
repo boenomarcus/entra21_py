ce16595cd0a0817f9702ad7a1d432c3fdaac21bf
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-10-06 18:54:36 -0300

    Update selection_sort.py

--- algorithms_mit/selection_sort.py
+++ algorithms_mit/selection_sort.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
 
     # Declare a list, sort it and present results for example purposes
     A = [6, 4, 5, 2.4, 7.5, 10, 7, 4, 9, 8,]
-    print("\n>> Merge Sort Example:")
+    print("\n>> Selection Sort Example:")
     print(f"\nOriginal List: {A}")
     print(f"Sorted List: {selection_sort(A)}\n")
