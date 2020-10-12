16d2eb08944b3668222f6c6bc9436a17cb4d1d35
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-10-12 17:27:18 -0300

    Exercícios 2 e 4

--- exercicios_aula/aula_008_def_dict/funcoes_endereco.py
+++ exercicios_aula/aula_008_def_dict/funcoes_endereco.py
@@ -1,16 +1,59 @@
+# Cria lista para armazenar informacoes de endereco
+# enderecos = []
+enderecos = [
+    {
+        "id": 0, 
+        "rua": "Antônio da Veiga", 
+        "numero": "200", 
+        "complemento": "Apto 104",
+        "bairro": "Victor Konder",
+        "cidade": "Blumenau",
+        "estado": "SC"
+    },
+    {
+        "id": 1, 
+        "rua": "General Osorio", 
+        "numero": "1890", 
+        "complemento": "Apto 1007",
+        "bairro": "Velha",
+        "cidade": "Blumenau",
+        "estado": "SC"
+    },
+    {
+        "id": 2, 
+        "rua": "Gustavo Zimmerman", 
+        "numero": "980", 
+        "complemento": "Apto 306",
+        "bairro": "Itoupava Central",
+        "cidade": "Blumenau",
+        "estado": "SC"
+    }
+]
 
-enderecos = []
 
-def cadastrar_endereco(id_pessoa, rua, numero, complemento, bairro, cidade, estado):
-    #--- Exercício 2  - Funções
-    #--- Escreva uma função para cadastro de endereço:
-    #---       a função deve receber sete parâmetros, id da pessoa, rua, numero, complemento, bairro, cidade e estado
-    #---       a função deve salvar os dados de endereço em uma lista com escopo global
-    #---       a função deve permitir o cadastro apenas de endereços com todos os dados preenchidos (str vazia ou None)
-    #---       a função deve retornar uma mensagem ao final de acordo com a situação
-    #--- A função deve ser salva em um arquivo diferente do arquivo principal onde será chamada
-    if id_pessoa == "" or rua == "" or numero == "" or complemento == "" or bairro == "" or cidade == "" or estado == "":
-        return "Cadastro de endereco falhou, faltam dados!"
+def cadastrar_endereco(
+        id_pessoa:int, rua:str, numero:str, complemento:str, 
+        bairro:str, cidade:str, estado:str) -> str:
+    """Cadastro de Endereço.
+
+    > Argumentos:
+        - id_pessoa (int): ID do cliente;
+        - rua (str): Nome da Rua;
+        - numero (str): Número do imóvel onde o cliente reside;
+        - complemento (str): Complemento ao número do imóvel (ex: Apt);
+        - bairro (str): Bairro onde o cliente reside;
+        - cidade (str): Cidade onde o cliente reside;
+        - estado (str): Estado onde o cliente reside.
+    
+    > Output
+        - Mensagem (str) indicando se o cadastro foi realizado ou não.
+    """
+    # Cria lista com parametros para testar condicao
+    params = [id_pessoa, rua, numero, complemento, bairro, cidade, estado]
+
+    # Testa se todos os dados estão preenchidos antes de realizar o cadastro
+    if "" in params or None in params:
+        return 0, "Cadastro de endereco falhou, faltam dados!"
     else:
         endereco = {}
         endereco["id"] = id_pessoa
@@ -21,20 +64,31 @@ def cadastrar_endereco(id_pessoa, rua, numero, complemento, bairro, cidade, esta
         endereco["cidade"] = cidade
         endereco["estado"] = estado
         enderecos.append(endereco)
-        return "Endereço cadastrado com sucesso!"
+        return 1, "Cadastrado realizado com sucesso!"
 
-def exibir_enderecos():
-    #--- Escreva uma função para listar endereços cadastrados:
-    #---    a função deve exibir todos os endereços cadastrados na função do ex2
-    print("\n" + "*"*50)
-    print(f"{'PESSOAS CADASTRADAS':^50}")
-    print("*"*50 + "\n")
+
+def enderecos_cadastrados() -> list:
+    """Retorna todos os endereços cadastrados.
+    
+    > Argumentos:
+        - Sem argumentos.
+    
+    > Output
+        - Lista (list) de dicionários com endereços cadastrados.
+    """
+    return enderecos
+
+
+def endereco_cliente(id_pessoa:int) -> dict:
+    """Retorna endereço de acordo com ID.
+    
+    > Argumentos:
+        - id_pessoa (int): ID do cliente que se deseja o endereço.
+    
+    > Output
+        - Dicionário (dict) com o endereco do cliente indicado.
+    """
+    # Retorn endereco de acordo com ID
     for endereco in enderecos:
-        print(f"[ID: {endereco['id']}] ")
-    print("\n" + "*"*50 + "\n")
-
-def exibir_endereco():
-    #--- Escreva uma função para exibir um endereço específico:
-    #        a função deve exibir um endereço cadastrado na função do ex2 filtrando por 
-    #        id da pessoa
-    pass
+        if endereco['id'] == id_pessoa:
+            return endereco
