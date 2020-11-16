bd02ca06ca270707c68533afe49e9d5a1929ab0f
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-11-16 13:16:15 -0300

    OS Class and Methods

---
+++ aulas_bruno/20201113_poo_extra/computer_utils/software/os_build.py
@@ -0,0 +1,116 @@
+"""
+Object-Oriented Programming (OOP)
+    - Operational System Class and Methods
+
+Blusoft/Senac - Formação em Python Entra21 2020
+
+Author: Marcus Moresco Boeno
+Last Update: 2020-11-16
+
+"""
+
+class OS_Build:
+    """Operational System (OS) Class
+
+    > Class Parameters:
+        - No paramaters.
+    """
+    # Constructor
+    def __init__(self, company:str, name:str, version:str):
+        self.__company = company
+        self.__name = name
+        self.__version = version
+    
+    @property
+    def company(self) -> str:
+        """OS Company Getter
+
+        > Arguments:
+            - No arguments.
+        
+        > Output:
+            - (str): Private instance attribute for os producer.
+        """
+        return self.__os_company
+    
+    @property
+    def name(self) -> str:
+        """OS Name Getter
+
+        > Arguments:
+            - No arguments.
+        
+        > Output:
+            - (str): Private instance attribute for os name.
+        """
+        return self.__os_name
+    
+    @property
+    def version(self) -> str:
+        """OS Version Getter
+
+        > Arguments:
+            - No arguments.
+        
+        > Output:
+            - (str): Private instance attribute for os version.
+        """
+        return self.__os_version
+    
+    @company.setter
+    def company(self, c:str) -> str:
+        """OS Company Setter
+
+        > Arguments:
+            - c (str): OS Company.
+        
+        > Output:
+            - No output.
+        """
+        self.__os_company = c
+    
+    @name.setter
+    def name(self, n:str) -> str:
+        """OS Name Setter
+
+        > Arguments:
+            - n (str): OS Name.
+        
+        > Output:
+            - No output.
+        """
+        self.__os_name = n
+
+    @version.setter
+    def version(self, v:str) -> str:
+        """OS Version Setter
+
+        > Arguments:
+            - v (str): OS Version.
+        
+        > Output:
+            - No output.
+        """
+        self.__os_version = v
+    
+    # Represent OS Info as a string
+    def __str__(self) -> str:
+        """String representation of the OS_Build instance
+
+        > Arguments:
+            - No arguments.
+        
+        > Output:
+            - (str): OS info as a string
+        """
+        os_info = "OS Info:\n"
+        os_info += f"   - Company: {self.__company}\n"
+        os_info += f"   - Name: {self.__name}\n"
+        os_info += f"   - Version: {self.__version}"
+        return os_info
+
+if __name__ == "__main__":
+
+    # Create Instance of an OS and print info on it
+    my_os = OS_Build("Microsoft", "Windows 10", "Pro (Build:1909HS)")
+    print(my_os)
