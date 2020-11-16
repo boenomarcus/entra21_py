f0574d72e23300ee02842dcad663fba99122fa86
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-11-16 14:12:24 -0300

    RAM Class and Methods

---
+++ aulas_bruno/20201113_poo_extra/computer_utils/hardware/ram.py
@@ -0,0 +1,129 @@
+"""
+Object-Oriented Programming (OOP)
+    - Random Acces Memory (RAM) Class and Methods
+
+Blusoft/Senac - Formação em Python Entra21 2020
+
+Author: Marcus Moresco Boeno
+Last Update: 2020-11-16
+
+"""
+
+class RAM:
+    """Random Access Memory (RAM) Class
+
+    > Class Parameters:
+        - No paramaters.
+    """
+    # Constructor
+    def __init__(
+        self, manufacturer:str, capacity:int, generation:str, clock_rate:int
+        ):
+        self.__manufacturer = manufacturer
+        self.__capacity = capacity
+        self.__generation = generation
+        self.__clock_rate = clock_rate
+    
+    @property
+    def manufacturer(self) -> str:
+        """RAM Manufacturer Getter
+
+        > Arguments:
+            - No arguments.
+
+        > Output:
+            - (str): Manufacturer brand name. 
+        """
+        return self.__manufacturer_getter
+    
+    @property
+    def capacity(self) -> int:
+        """RAM Capacity Getter
+
+        > Arguments:
+            - No arguments.
+
+        > Output:
+            - (int): Capacity (storage) of the RAM.
+        """
+        return self.__capacity_getter
+
+    @property
+    def generation(self) -> str:
+        """RAM Generation Getter
+
+        > Arguments:
+            - No arguments.
+
+        > Output:
+            - (str): Generation (DDR3, DDR4) of the RAM. 
+        """
+        return self.__generation_getter
+    
+    @property
+    def clock_rate(self) -> int:
+        """RAM Clock Rate Getter
+
+        > Arguments:
+            - No arguments.
+
+        > Output:
+            - (int): RAM Clock rate.
+        """
+        return self.__clock_rate_getter
+    
+    @manufacturer.setter
+    def manufacturer(self, manu:str):
+        """RAM Manufacturer Setter
+
+        > Arguments:
+            - manu (str): Manufacturer brand name.
+        
+        > Output:
+            - No output.
+        """
+        self.__manufacturer_getter = manu
+    
+    @capacity.setter
+    def capacity(self, cap:int):
+        """RAM Capacity Setter
+
+        > Arguments:
+            - cap (int): Capacity (storage) of the RAM.
+        
+        > Output:
+            - No output.
+        """
+        self.__capacity_getter = cap
+    
+    @generation.setter
+    def generation(self, gen:str):
+        """RAM Generation Setter
+
+        > Arguments:
+            - gen (str): Generation (DDR3, DDR4) of the RAM.
+        
+        > Output:
+            - No output.
+        """
+        self.__generation_getter = gen
+    
+    @clock_rate.setter
+    def clock_rate(self, rate:int):
+        """RAM Clock Rate Setter
+
+        > Arguments:
+            - rate (int): RAM Clock rate.
+        
+        > Output:
+            - No output.
+        """
+        self.__clock_rate_getter = cap
+    
+    # Represent RAM stick info as a string
+    def __str__(self):
+        res = f"{self.__manufacturer}"
+        res += f" {self.__capacity}GB"
+        res += f" {self.__generation}"
+        res += f" {self.__clock_rate} MHz RAM Module"
+        return res
